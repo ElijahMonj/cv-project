5e91fb9fb2560a902a40d5729636fa60cb860bd2
Elijah Monjardin <monjardinelijah120@gmail.com>
2022-09-17 23:05:42 +0800

    Formatted CV Preview

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
             <input id='name'></input>
         </div>
         <div className='showInfo'>
+        
           <div className='cvPreview'>
             <div className='basicInfo'>
               <img src={avatar} className="avatar" alt="avatar"/>
@@ -21,8 +22,44 @@ function App() {
                   <div className='phone'>Phone : {'1212121212'}</div>
                   <div className='email'>E-mail: {'sample@gmail.com'}</div>
               </div>
-              
             </div>
+            <div className='personalInfo'>
+                <div className='aboutMe'>About Me</div><br></br>
+                {"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}
+            </div>
+            <div className='education'>
+                <div className='aboutMe'>Education</div><br></br>
+                <ul>
+                  
+                </ul>
+             </div>
+             <div className='skills'>
+                <div className='aboutMe'>Skills</div><br></br>
+                <ul>
+                  <li>as</li>
+                  <li>as</li>
+                  <li>as</li>
+                  <li>as</li>
+                </ul>
+             </div>
+             <div className='experience'>
+                <div className='aboutMe'>Experience</div><br></br>
+                <ul>
+                  <li>as</li>
+                  <li>as</li>
+                  <li>as</li>
+                  <li>as</li>
+                </ul>
+             </div>
+             <div className='certifications'>
+                <div className='aboutMe'>Certifications</div><br></br>
+                <ul>
+                  <li>as</li>
+                  <li>as</li>
+                  <li>as</li>
+                  <li>as</li>
+                </ul>
+             </div>
             
           </div>
         </div>
