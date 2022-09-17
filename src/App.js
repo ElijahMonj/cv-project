43806279fc0197e5a16f29b2a037217b1fccb55d
Elijah Monjardin <monjardinelijah120@gmail.com>
2022-09-17 15:40:07 +0800

    started preview template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import avatar from './avatar.svg';
 import './App.css';
+import './cvstyles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <header>Make your CV</header>
+      <div className='content'>
+        <div className='addInfo'>
+            <label htmlFor='name'>Name</label>
+            <input id='name'></input>
+        </div>
+        <div className='showInfo'>
+          <div className='cvPreview'>
+            <div className='basicInfo'>
+              <img src={avatar} className="avatar" alt="avatar"/>
+              <div className='basicInfoDetails'>
+                  <div className='name'>{"Elijah Monjardin"}</div>
+                  <div className='jobTitle'>{"IT Project Manager"}</div>
+                  <div className='phone'>Phone : {'1212121212'}</div>
+                  <div className='email'>E-mail: {'sample@gmail.com'}</div>
+              </div>
+              
+            </div>
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
